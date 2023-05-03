261de50b5b068f6f17242cf4b84ab65ca2ed304d
Alice Lerias <leriasler@gmail.com>
2023-05-03 14:31:04 -0300

    Infinite scroll on feed

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,6 +1,6 @@
 import { BoxPosts } from "./BoxPosts";
 import { MenuPosts } from "./MenuPosts";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import { NavigateFunction } from "react-router-dom";
 import { Post } from "../types";
 import { useInfiniteQuery } from "react-query";
@@ -26,13 +26,25 @@ export const FeedComponent: React.FC<PropsWithChildren<props>> = ({
       (previous, current) => [...previous, ...current.content],
       [] as Post[]
     ) || [];
+
+  useEffect(() => {
+    if (!window.IntersectionObserver) return;
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.isIntersecting)) {
+        fetchNextPage();
+      }
+    });
+    intersectionObserver.observe(document.querySelector("#observer")!);
+    return () => intersectionObserver.disconnect();
+  }, []);
+
   return (
     <BoxPosts
       isLoading={isLoading}
       data={posts}
       navigate={navigate}
       children={<MenuPosts />}
-      button={<button onClick={() => fetchNextPage()}>More</button>}
+      button={<li id="observer" className="text-black"></li>}
     />
   );
 };
