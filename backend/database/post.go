c74b3d531c8f4f189ed8139e9a606270e15770bc
Alice Lerias <leriasler@gmail.com>
2023-05-08 12:20:54 -0300

    fix queries

--- backend/database/post.go
+++ backend/database/post.go
@@ -1,23 +1,13 @@
 package database
 
 import (
-	"errors"
-
 	"github.com/alicelerias/blog-golang/models"
-	"github.com/jinzhu/gorm"
 )
 
 func (s *PostgresDBRepository) CreatePost(post *models.Post) error {
-	err := s.db.Debug().Create(&post).Error
+	err := s.db.Preload("Author").Create(&post).Error
 	if err != nil {
 		return err
-	}
-
-	if post.ID != 0 {
-		err = s.db.Debug().Model(&models.User{}).Where("id = ?", post.ID).Take(&post.Author).Error
-		if err != nil {
-			return err
-		}
 	}
 	return nil
 }
@@ -39,18 +29,12 @@ func (s *PostgresDBRepository) GetPosts(cursor string, post *models.Post) ([]mod
 	return posts, nil
 }
 
-func (s *PostgresDBRepository) GetPost(id string) (post *models.Post, err error) {
-	post = &models.Post{}
-	err = s.db.First(post, id).Error
-	if gorm.IsRecordNotFoundError(err) {
-		err = errors.New("Post Not Found")
+func (s *PostgresDBRepository) GetPost(id string) (*models.Post, error) {
+	post := &models.Post{}
+	if err := s.db.Preload("Author").First(post, id).Error; err != nil {
+		return &models.Post{}, err
 	}
-	if post.ID != 0 {
-		err = s.db.Debug().Model(&models.Post{}).Where("id = ?", post.AuthorID).Take(&post.Author).Error
-		if err != nil {
-			return &models.Post{}, err
-		}
-	}
+
 	return post, nil
 
 }
@@ -72,24 +56,18 @@ func (s *PostgresDBRepository) GetPostsByUser(post *models.Post, cursor string, 
 	return posts, nil
 }
 
-func (s *PostgresDBRepository) UpdatePost(values interface{}, id string) (post *models.Post, err error) {
-	post = &models.Post{}
-	err = s.db.Table("posts").Where("id = ?", id).UpdateColumns(values).Take(post).Error
-	if gorm.IsRecordNotFoundError(err) {
-		err = errors.New("Post Not Found")
+func (s *PostgresDBRepository) UpdatePost(values interface{}, id string) (*models.Post, error) {
+	post := &models.Post{}
+	if err := s.db.Preload("Author").Table("posts").Where("id = ?", id).UpdateColumns(values).Take(post).Error; err != nil {
+		return &models.Post{}, err
 	}
-	if post.ID != 0 {
-		err = s.db.Debug().Model(&models.Post{}).Where("id = ?", post.AuthorID).Take(&post.Author).Error
-		if err != nil {
-			return
-		}
-	}
-	return
+
+	return post, nil
 
 }
 func (s *PostgresDBRepository) DeletePost(id string) error {
 	post := &models.Post{}
-	err := s.db.Debug().Delete(post, id).Error
+	err := s.db.Delete(post, id).Error
 	if err != nil {
 		return err
 	}
