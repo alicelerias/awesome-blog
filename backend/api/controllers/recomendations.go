4fe6cb72503f223945c4f929a5bf90f6cad9b138
Alice Lerias <leriasler@gmail.com>
2023-05-08 16:19:45 -0300

    recomendations query

--- backend/api/controllers/recomendations.go
+++ backend/api/controllers/recomendations.go
@@ -3,20 +3,33 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/alicelerias/blog-golang/types"
 	"github.com/gin-gonic/gin"
 )
 
 func (server *Server) GetRecomendations(ctx *gin.Context) {
+	// cache := []*types.User{}
+	// name := "users"
+	// nameSpace := "all"
 	uid, exists := ctx.Get("uid")
 	if !exists {
 		ctx.JSON(http.StatusForbidden, gin.H{"error": "problem to authenticate user"})
 		return
 	}
+	// if err := server.cache.GetKey(name, nameSpace, &cache); err == nil {
+	// 	ctx.JSON(http.StatusOK, gin.H{"users": cache})
+	// } else {
 	recomendations, err := server.repository.Recomendations(uid.(string))
 	if err != nil {
 		ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-
-	ctx.JSON(http.StatusOK, gin.H{"users": recomendations})
+	fromModelUsers := []*types.User{}
+	for _, item := range *recomendations {
+		newItem := server.userFromModel(&item, uid.(string))
+		fromModelUsers = append(fromModelUsers, newItem)
+	}
+	ctx.JSON(http.StatusOK, gin.H{"users": fromModelUsers})
+	// server.cache.SetKey(name, nameSpace, fromModelUsers, time.Hour)
+	// }
 }
--- backend/database/recomendations.go
+++ backend/database/recomendations.go
@@ -5,44 +5,42 @@ import (
 )
 
 func (s *PostgresDBRepository) Recomendations(uid string) (*[]models.User, error) {
+	// report := []struct {
+	// 	column_a string `gorm:columa`
+	// }
 	users := []models.User{}
-	friendsOfFriends := []models.User{}
+
 	recomendations := []models.User{}
 
 	// rever
 
 	err := s.db.Raw(
-		`select distinct recomended_user.id, recomended_user.user_name, recomended_user.avatar, recomended_user.bio, pop.score from users as recomended_user
-		join popularity_score as pop
-		on recomended_user.id = pop.id
-		join followings as recomendations
-		on pop.id = recomendations.following_id
-		join users as friend
-		on recomendations.follower_id = friend.id
-		join followings as my_friends
-		on friend.id = my_friends.following_id
-		join users as me 
-		on my_friends.follower_id = me.id
-		where me.id = ? and recomendations.following_id != me.id
+		`select distinct recomended_user.id, recomended_user.user_name, recomended_user.avatar, recomended_user.bio, pop.score
+		from users as recomended_user
+		join popularity_score as pop on recomended_user.id = pop.id
+		join followings as recomendations on pop.id = recomendations.following_id
+		join users as friend on recomendations.follower_id = friend.id
+		join followings as my_friends on friend.id = my_friends.following_id
+		join users as me on my_friends.follower_id = me.id and me.id = ?
+		where recomendations.following_id != me.id and recomendations.follower_id != me.id
 		order by pop.score DESC
-		limit 5;
+		limit 10;
 		`, uid).
-		Find(&friendsOfFriends).Error
+		Find(&users).Error
 	if err != nil {
 		return &[]models.User{}, err
 	}
 
-	s.db.Raw(
-		`SELECT profile.id, profile.user_name, profile.bio, profile.avatar
-		FROM popularity_score AS pop
-			JOIN users AS profile
-				ON pop.id = profile.id
-		ORDER BY pop.score DESC
-		LIMIT 5`).
-		Find(&recomendations)
+	if len(users) < 10 {
+		s.db.Raw(
+			`select * from popularity_score as pop
+			join users as u on pop.id = u.id
+			order by pop.score DESC
+			`).Limit(10 - len(users)).
+			Find(&recomendations)
 
-	friendsOfFriends = append(friendsOfFriends, recomendations...)
-	users = append(users, friendsOfFriends...)
+		users = append(users, recomendations...)
+	}
 
 	return &users, nil
 }
